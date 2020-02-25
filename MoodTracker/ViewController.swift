2802744e2fcba9fd3c63d80731e6bd2cf0a48f06
Claudia Maciel <clc_80@yahoo.com>
2020-02-24 20:05:16 -0800

    Initial Commit

---
+++ MoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by Claudia Contreras on 2/24/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
